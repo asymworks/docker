ceadfc3a1d28a867edc537c7e4cb10fc4b157bb5
Jonathan Krauss <jkrauss@asymworks.com>
2021-05-04 16:23:49 -0700

    Updates to qcadoo Dockerfiles

--- qcadoo/entrypoint.sh
+++ qcadoo/entrypoint.sh
@@ -3,11 +3,11 @@
 # Qcadoo Docker Entrypoint
 # Copyright (c) 2021 Asymworks, LLC
 
-DB_HOST=${DB_HOST:-db}
-DB_PORT=${DB_PORT:-5432}
-DB_NAME=${DB_NAME:-mes}
-DB_USERNAME=${DB_USERNAME:-postgres}
-DB_PASSWORD=${DB_PASSWORD:-postgres123}
+export PGDB_HOST=${QCADOO_DB_HOST:-db}
+export PGDB_PORT=${QCADOO_DB_PORT:-5432}
+export PGDB_NAME=${QCADOO_DB_NAME:-mes}
+export PGDB_USERNAME=${QCADOO_DB_USERNAME:-qcadoo}
+export PGDB_PASSWORD=${QCADOO_DB_PASSWORD:-qcadoo}
 
 abort() {
     local msg=$1
@@ -21,22 +21,35 @@ info() {
     echo -e "\033[32m[INFO]\033[0m $1"
 }
 
+pg_wait() {
+    # Wait for the PostgreSQL connection to be available
+    pg_isready -q -h ${PGDB_HOST} -p ${PGDB_PORT} && return
+    info "Waiting for PostgreSQL connection"
+    for run in {1..10} ; do
+        sleep 3
+        pg_isready -q -h ${PGDB_HOST} -p ${PGDB_PORT} && break
+    done
+    [ $? -eq 0 ] || abort "Timed out waiting for PostgreSQL connection"
+}
+
 if [ "$1" == "start" ] ; then
     # Write environment variables into Qcadoo Configuration
     [ -f db.properties.conf ] || abort "Missing db.properties.conf file"
     info "Updating database connection information in db.properties"
     envsubst < db.properties.conf > $HOME/mes-application/qcadoo/db.properties
 
-    # Wait for the PostgreSQL connection to be available
-    info "Waiting for PostgreSQL connection"
-    for run in {1..10} ; do
-        pg_isready -h ${DB_HOST} -p ${DB_PORT} -d ${DB_NAME} && break
-    done
-    [ $? -eq 0 ] || abort "Timed out waiting for PostgreSQL connection"
-
     # Start the Qcadoo Server
+    pg_wait
     info "Starting Qcadoo"
     exec $HOME/mes-application/bin/startup.sh
+fi
+
+if [ "$1" == "create-schema" ]; then
+    pg_wait
+    info "Loading SQL Schema"
+    SQL_FILE="${HOME}/mes-application/webapps/ROOT/WEB-INF/classes/schema/demo_db_en.sql"
+    PSQL="psql -h ${PGDB_HOST} -p ${PGDB_PORT} -U ${PGDB_USERNAME} ${PGDB_NAME}"
+    PGPASSWORD="${PGDB_PASSWORD}" exec $PSQL < $SQL_FILE
 fi
 
 exec "$@"
