f5d600e1271ff7067262c50350f1327a2ce72f31
J.P. Krauss <jkrauss@asymworks.com>
2021-05-04 12:43:57 -0700

    Optimize Entrypoint

--- qcadoo/entrypoint.sh
+++ qcadoo/entrypoint.sh
@@ -21,32 +21,22 @@ info() {
     echo -e "\033[32m[INFO]\033[0m $1"
 }
 
-check_configuration() {
+if [ "$1" == "start" ] ; then
     # Write environment variables into Qcadoo Configuration
     [ -f db.properties.conf ] || abort "Missing db.properties.conf file"
     info "Updating database connection information in db.properties"
     envsubst < db.properties.conf > $HOME/mes-application/qcadoo/db.properties
-}
 
-wait_for_postgres() {
     # Wait for the PostgreSQL connection to be available
     info "Waiting for PostgreSQL connection"
     for run in {1..10} ; do
         pg_isready -h ${DB_HOST} -p ${DB_PORT} -d ${DB_NAME} && break
     done
     [ $? -eq 0 ] || abort "Timed out waiting for PostgreSQL connection"
-}
 
-startup() {
     # Start the Qcadoo Server
     info "Starting Qcadoo"
-    $HOME/mes-application/bin/startup.sh
-}
-
-if [ "$1" == "start" ] ; then
-    check_configuration
-    wait_for_postgres
-    startup
+    exec $HOME/mes-application/bin/startup.sh
 fi
 
-exec $@
+exec "$@"
